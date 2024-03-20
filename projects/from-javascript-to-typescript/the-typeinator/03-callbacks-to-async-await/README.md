8c84210168d1625bc47a38585de7a466f587b21b
미플 <devmeeple@gmail.com>
2024-03-20 23:28:47 +0900

    refactor: callbacks to async await
    - README 번역
    - 콜백 함수 async/await 구문 적용

--- projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/README.md
+++ projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/README.md
@@ -4,8 +4,14 @@ Your operational results are within the expected parameters.
 Time for the last project.
 
 The [`original.js`](./original.js) file contains the project's original code for `checkEmotion` and `speak` functions.
+
+> 프로젝트에 [`original.js`](./original.js) 파일은 원본코드 `checkEmotion`, `speak` 함수를 포함합니다.
+
 In this time period, those functions are written with traditional Node-style callback parameters.
 Your job is to port that code to proper ES2017+ `async`/`await`.
+
+> - 이번 시간에는 기존 함수를 전통적인 Node-style callback 매개변수로 작성합니다.
+> - 당신이 할 일은 ES2017+ `async/await` 코드로 변환해야 합니다.
 
 ## Specification
 
--- projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
@@ -1,5 +1,21 @@
 // Put your checkEmotion and speak functions here! ✨
 // See ./original.js for their older JavaScript code.
+async function checkEmotion(knownEmotions, emotion) {
+	// 데이터베이스 처리 시뮬레이션
+	await new Promise((resolve) => {
+		return setTimeout(resolve, 1000);
+	});
+
+	return knownEmotions.has(emotion);
+}
+
+async function speak(knownEmotions, newEmotion, phrase) {
+	if (!(await checkEmotion(knownEmotions, newEmotion, phrase))) {
+		throw new Error(`Does not compute. I do not understand ${newEmotion}.`);
+	}
+
+	return `"${phrase}" (${newEmotion})`;
+}
 
 module.exports.checkEmotion = checkEmotion;
 module.exports.speak = speak;
